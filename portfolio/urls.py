bf178b0e4ef7736ce120c5ddec3d21a2d576494d
Gustavo Rivero <gusir22@gmail.com>
2025-02-06 20:17:54 -0500

    Create PortfolioListView

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,5 +1,9 @@
 # portfolio/urls.py
 from django.urls import path
 
+from .views import PortfolioListView
 
-urlpatterns = []
+
+urlpatterns = [
+    path("", PortfolioListView.as_view(), name="portfolio_list"),
+]
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,4 +1,12 @@
 # portfolio/views.py
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class PortfolioListView(TemplateView):
+    template_name = "portfolio/portfolio-list.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # query Featured Projects here
+        # query Quick Projects here
+        return context
